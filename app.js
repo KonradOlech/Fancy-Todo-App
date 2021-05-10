0840e5108b409496f153e69e6e60de2e34326a59
Konrad Olech <frytas93@gmail.com>
2021-05-10 21:53:23 +0200

    Minor changes in naming

--- app.js
+++ app.js
@@ -1,173 +1,192 @@
 // Selectors
 const newTodoInput = document.getElementById('newTodoInput');
-const todoList = document.getElementById('todoList')
-
+const todoList = document.getElementById('todoList');
 
 let todos = [];
 
 function createTodo(todosArr, input) {
-    const newTodo = {
-        id: generateId(todosArr),
-        isDone: false,
-        content: input.value
-    }
+	const newTodo = {
+		id: generateId(todosArr),
+		isDone: false,
+		content: input.value,
+	};
 
-    todosArr.push(newTodo)
-    // state.setState('all')
-    state.setState(state.state)
-    showRemainingCounter(todosArr)
-
+	todosArr.push(newTodo);
+	saveTodos(newTodo);
+	state.setState(state.currentState);
+	showRemainingCounter(todosArr);
 }
 
 function getTodo(input) {
-    const id = Number(input.dataset.todoId);
-    const isDone = input.querySelector('.checkbox').checked;
-    const content = input.querySelector('.newTodoInput').value;
+	const id = Number(input.dataset.todoId);
+	const isDone = input.querySelector('.checkbox').checked;
+	const content = input.querySelector('.newTodoInput').value;
 
-    return {id, isDone, content}
-
+	return { id, isDone, content };
 }
 
 function reloadTodos(todosArr) {
-    todoList.innerHTML = ''
-    todosArr.forEach(todo => buildTodoComponent(todo))
+	todoList.innerHTML = '';
+	todosArr.forEach((todo) => buildTodoComponent(todo));
 }
 
 function generateId(arr) {
-    const allId = arr.map(i => i.id);
-    let newId;
-    do {
-        newId = Math.floor(Math.random() * 256);
-    } while (allId.includes(newId));
+	const allId = arr.map((i) => i.id);
+	let newId;
+	do {
+		newId = Math.floor(Math.random() * 256);
+	} while (allId.includes(newId));
 
-    return newId
+	return newId;
 }
 
 function deleteTodo(item, todosArr) {
-    todos = todosArr.filter(x => x.id !== item.id);
-     showRemainingCounter(todos)
+	todos = todosArr.filter((x) => x.id !== item.id);
+	showRemainingCounter(todos);
 }
 
-function deleteCompletedTodos(todosArr) {
-    const newTodos = filterTodos(todosArr, 'active');
-    console.log(newTodos)
-}
+// function deleteCompletedTodos(todosArr) {
+//     const newTodos = filterTodos(todosArr, 'active');
+//     console.log(newTodos)
+// }
 
 function changeTodo(item, todosArr) {
-    todos = todosArr.map(todo => (todo.id === item.id) ? item : todo);
-    showRemainingCounter(todos)
+	todos = todosArr.map((todo) => (todo.id === item.id ? item : todo));
+	state.setState(state.currentState);
+	showRemainingCounter(todos);
 }
 
 function showRemainingCounter(todosArr) {
-    const uncompletedTodos = todosArr.filter(todo => todo.isDone === false);
-    const counter = document.getElementById('remainingCounter');
-    counter.innerText = `${uncompletedTodos.length} items left`
-
+	const uncompletedTodos = todosArr.filter((todo) => todo.isDone === false);
+	const counter = document.getElementById('remainingCounter');
+	counter.innerText = `${uncompletedTodos.length} items left`;
 }
 
 function filterTodos(todosArr, filter) {
-    let filteredTodos;
-    switch (filter) {
-        case 'all':
-            filteredTodos = todosArr;
-            break;
-        
-        case 'active':
-            filteredTodos = todosArr.filter(todo => todo.isDone === false);
-            break;
-        
-        case 'completed':
-            filteredTodos = todosArr.filter(todo => todo.isDone === true);
-            break;
-    }
+	let filteredTodos;
+	switch (filter) {
+		case 'all':
+			filteredTodos = todosArr;
+			break;
 
-    return filteredTodos
+		case 'active':
+			filteredTodos = todosArr.filter((todo) => todo.isDone === false);
+			break;
+
+		case 'completed':
+			filteredTodos = todosArr.filter((todo) => todo.isDone === true);
+			break;
+	}
+
+	return filteredTodos;
 }
-
-
-
 
 function buildTodoComponent(props) {
-    const { id, isDone, content } = props;
-    const todo = document.createElement('div');
-    todo.classList.add('todo-item');
-    todo.dataset.todoId = id;
+	const { id, isDone, content } = props;
+	const todo = document.createElement('div');
+	todo.classList.add('todo-item');
+	todo.dataset.todoId = id;
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.value = 'done'
-    checkbox.defaultChecked = isDone;
-    checkbox.classList.add('checkbox');
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.value = 'done';
+	checkbox.defaultChecked = isDone;
+	checkbox.classList.add('checkbox');
 
-    checkbox.addEventListener('click', e => changeTodo(getTodo(e.target.parentNode), todos))
+	checkbox.addEventListener('click', (e) =>
+		changeTodo(getTodo(e.target.parentNode), todos),
+	);
 
-    const text = document.createElement('input');
-    text.type = 'text';
-    text.value = content;
-    text.classList.add('newTodoInput');
+	const text = document.createElement('input');
+	text.type = 'text';
+	text.value = content;
+	text.classList.add('newTodoInput');
 
-    text.addEventListener('keydown', e => {
-        if (e.key === "Enter") {
-            changeTodo(getTodo(e.target.parentNode), todos)
-        }
-    })
+	text.addEventListener('keydown', (e) => {
+		if (e.key === 'Enter') {
+			changeTodo(getTodo(e.target.parentNode), todos);
+		}
+	});
 
-    const deleteBtn = document.createElement('a');
-    deleteBtn.classList.add('deleteBtn');
+	const deleteBtn = document.createElement('a');
+	deleteBtn.classList.add('deleteBtn');
 
-    deleteBtn.addEventListener('click', e => {
-        const i = (getTodo(e.target.parentNode));
-        console.log(i, todos)
-        deleteTodo(i, todos)
-        state.setState(state.state)
-    })
+	deleteBtn.addEventListener('click', (e) => {
+		const i = getTodo(e.target.parentNode);
+		deleteTodo(i, todos);
+		state.setState(state.currentState);
+	});
 
-    todo.appendChild(checkbox);
-    todo.appendChild(text);
-    todo.appendChild(deleteBtn);
+	todo.appendChild(checkbox);
+	todo.appendChild(text);
+	todo.appendChild(deleteBtn);
 
-    todoList.appendChild(todo)
+	todoList.appendChild(todo);
 }
 
-newTodoInput.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-        createTodo(todos, e.target)
-        e.target.value = ''
-    }
-})
+newTodoInput.addEventListener('keydown', (e) => {
+	if (e.key === 'Enter') {
+		createTodo(todos, e.target);
+		e.target.value = '';
+	}
+});
 
 const filterSelectors = document.querySelectorAll('.filterOption');
-console.log(filterSelectors)
+console.log(filterSelectors);
 
-filterSelectors.forEach(selector => {
-    selector.addEventListener("click", e => state.setState(e.target.dataset.filter))
-})
-
+filterSelectors.forEach((selector) => {
+	selector.addEventListener('click', (e) =>
+		state.setState(e.target.dataset.filter),
+	);
+});
 
 const clearCompleted = document.getElementById('clearCompleted');
-clearCompleted.addEventListener('click', e => {
-    todos = filterTodos(todos, 'active')
-    reloadTodos(todos)
-} )
-
+clearCompleted.addEventListener('click', (e) => {
+	todos = filterTodos(todos, 'active');
+	reloadTodos(todos);
+	state.setState(state.currentState);
+});
 
 class State {
-    constructor() {
-        this.state = 'all'
-        this.stateOptions = ["all", "active", "completed"]
-    }
+	constructor() {
+		this.currentState = 'all';
+		this.stateOptions = ['all', 'active', 'completed'];
+	}
 
-    setState(value) {
-        console.log(this)
-        reloadTodos(filterTodos(todos, value));
-        this.state = value;
-        this.stateOptions.forEach(option => document.querySelector(`[data-filter="${option}"]`).classList.remove('filterSelected'));
-        document.querySelector(`[data-filter="${value}"]`).classList.add('filterSelected')
-
-    }
+	setState(value) {
+		reloadTodos(filterTodos(todos, value));
+		this.currentState = value;
+		this.stateOptions.forEach((option) =>
+			document
+				.querySelector(`[data-filter="${option}"]`)
+				.classList.remove('filterSelected'),
+		);
+		document
+			.querySelector(`[data-filter="${value}"]`)
+			.classList.add('filterSelected');
+	}
 }
 
 const state = new State();
 
+function saveTodos(newTodos) {
+	let todos;
+	if (localStorage.getItem('TodosData') === null) {
+		todos = [];
+	} else {
+		todos = JSON.parse(localStorage.getItem('TodosData'));
+	}
+	todos.push(newTodos);
+	localStorage.setItem('TodosData', JSON.stringify(todos));
+}
 
+function loadTodos() {
+	let todos;
+	if (localStorage.getItem('TodosData') === null) {
+		todos = [];
+	} else {
+		todos = JSON.parse(localStorage.getItem('TodosData'));
+	}
 
+	return todos;
+}
